7a1614fc627e451701c9a202af2e6cf7550f116e
kalkidan <kalkidanabreham214@gmail.com>
2024-12-09 06:03:31 +0300

    modification added

--- src/API/axiox.jsx
+++ src/API/axiox.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
 export const axiosInstant = axios.create({
-  // baseURL:"http://localhost:4444"
-  baseURL: "https://amazon-api-deploy-group-4.onrender.com",
+  //   baseURL:"http://localhost:4444"
+  baseURL: "https://amazon-api-deploy-group-4-1.onrender.com",
 });
